861437848aa14abc78dcd334b30b29b48bacb595
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-10-24 19:42:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,5 +13,16 @@ if(err) console.log(err)
 });
 //Command handler.
 
+bot.on('message', message => {
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray[0].slice(prefix.length))
+  if (!message.content.startsWith(prefix)) return;
+  if (cmd) {
+    cmd.run(bot, message, args, Discord)
+console.log(`${message.author.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+})
+
 
 bot.login(process.evl.token)
