0d8ce29c38d8a9255d15a344f82b6594823e42fb
Austin Gallippo <TheWolfStudios154@gmail.com>
2018-10-24 19:40:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,17 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
-
-const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
-
-if(!process.send) {
-
-Files.initStandalone();
-
-} else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
+const Discord = require("discord.js");
+const bot = new Discord.NewClient();
+const fs = require("fs");
+var prefix = ">"
+//Bot init
+bot.command = Discord.Collection()
+fs.readdir("./commands", (err, files) => {
+	console.log("Loading/Reading commands.")
+if(err) console.log(err)
+	 files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+		 bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+	 });
 });
+//Command handler.
 
-}
+
+bot.login(process.evl.token)
